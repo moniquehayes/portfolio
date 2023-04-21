c9d172ab49b15d17b217da97b35c78f3580d8ae3
Monique Hayes <moniqueehayes5@gmail.com>
2023-04-21 10:10:34 -0500

    tailwind installed

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
 import React from "react";
+import { Link, Routes, Route } from "react-router-dom";
+import About from "./features/About";
+import Contact from "./features/Contact";
+import Work from "./features/Work";
 
 const App = () => {
   return (
     <div className="App">
-      will import components here!
+      <nav>
+        <Link id='home' to='/'>Home</Link>
+        <Link id='about' to='/'>About</Link>
+        <Link id='work' to='/'>Work</Link>
+        <Link id='contact' to='/'>Contact</Link>
+      </nav>
+      <main>
+        {/** MAY NEED TO ALSO MAKE A SEPARATE HOME COMPONENT IF DON'T WANT IT AS APP */}
+        <Routes>
+          <Route path='/about' element={<About/>}></Route>
+          <Route path='/work' element={<Work/>}></Route>
+          <Route path='/contact' element={<Contact/>}></Route>
+        </Routes>
+      </main>
     </div>
   );
 }
---
+++ src/features/Work.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Work = () => {
+
+    return (
+        <>
+        <h1>Work</h1>
+        </>
+    )
+};
+
+export default Work;
