cc468dc018a90d2c37c13270d33e9d49211a8e62
Monique Hayes <moniqueehayes5@gmail.com>
2023-04-24 17:12:59 -0500

    bones done

--- src/features/About.js
+++ src/features/About.js
@@ -8,11 +8,20 @@ const About = () => {
         {/**ADD PHOTO */}
         <p>I'm a former Geometry Teacher turned Full Stack Software Engineer with an eye for detail and a passion for collaborating with others. I've always loved math, logic, and problem solving. After taking on a part-time Operations role during my last year of teaching, I realized I also loved logistics and data, and I wanted to harness this newfound love as I looked to make a career shift. After exploring SQL through an online Cornell course, along with some phenomenal guidance from a mentor of mine, I decided that a Software Engineering bootcamp, and more specifically, the Grace Hopper Program at Fullstack Academy, would be the perfect way to make this shift. Throughout the program I have loved discovering new languages and gaining the knowledge base to create full stack websites in just a few short months. I am most interested in Full Stack and Frontend Development roles. As I enter the job market, I am looking for a company that not only is working to make positive growth and change in the world, but furthermore is passionate about fostering community and growth amongst its employees.</p>
         <p>Please take a look at some of the projects I've done below!</p>
-        <button className="bg-sky-500 hover:bg-sky-700 ...">
-  Save changes
-</button>
-
-        {/** ADD CAROUSEL EQUIVALENT OR JUST LINKS WITH LITTLE LOGOS FOR PROJECTS BELOW */}
+        <div style={{display: 'flex', flexDirection: 'row', flexWrap: 'wrap', justifyContent: 'space-around'}}>
+            <div>
+                <h3>Yummify</h3>
+                <p>add pic and link</p>
+            </div>
+            <div>
+                <h3>Pick Me UP</h3>
+                <p>add pic and link</p>
+            </div>
+            <div>
+                <h3>Wild Roots</h3>
+                <p>add pic and link</p>
+            </div>
+        </div>
         </>
     )
 };
--- src/features/Work.js
+++ src/features/Work.js
@@ -3,19 +3,36 @@ import React from "react";
 const Work = () => {
   return (
     <>
-      <h1>Work</h1>
-      <main>
-        <div>
-          <h5>Wild Roots</h5>
-          <p>**description**</p>
+      <h1 style={{textAlign: 'center'}}>Work</h1>
+      {/* <main className="flex flex-wrap mx-auto"> */}
+      <main style={{display: 'flex', flexWrap: 'wrap', flexDirection: 'row', justifyContent: 'center'}}>
+        <div style={{border: '1px solid lightgrey', margin: '5px', padding: '3px', width: '20rem'}}>
+          <h2 style={{textAlign: 'center'}}>Yummify</h2>
+
+          <p>
+            A full stack application which works to reduce food waste by
+            providing an avenue for restaurants to sell their unsold meals
+            and/or items for discounted prices to consumers. Key technologies
+            used in development include Node.js, React, JavaScript, Firebase,
+            Redux, Bootstrap, Webpack, GoogleMapsAPI
+          </p>
         </div>
-        <div>
-          <h5>Pick me UP</h5>
-          <p>**description**</p>
+        <div style={{border: '1px solid lightgrey', margin: '5px', padding: '3px', width: '20rem'}}>
+          <h2 style={{textAlign: 'center'}}>Pick me UP</h2>
+          <p>
+            A frontend site that generates inspirational quotes for users,
+            utilizing 34 different categories with up to 10 quotes within each
+            category. Key technologies used in development include React,
+            JavaScript, Node.js, HTML5, CSS3
+          </p>
         </div>
-        <div>
-          <h5>Yummify</h5>
-          <p>**description**</p>
+        <div style={{border: '1px solid lightgrey', margin: '5px', padding: '3px', width: '20rem'}}>
+          <h2 style={{textAlign: 'center'}}>Wild Roots</h2>
+          <p>
+            A full stack ecommerce site for plants utilizing Node.js, React,
+            Redux, JavaScript, React Bootstrap, Stripe, Express, Webpack, and
+            Sequelize
+          </p>
         </div>
       </main>
     </>
