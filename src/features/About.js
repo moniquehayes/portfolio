ffe07376bd639c29822149261adf59170204a546
Monique Hayes <moniqueehayes5@gmail.com>
2023-05-02 16:06:39 -0500

    updated about section and styling

--- src/features/About.js
+++ src/features/About.js
@@ -1,36 +1,53 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const About = () => {
   return (
     <>
-      <div style={{display: 'flex', justifyContent: 'space-between', alignItems: 'center', margin: '10vh 0', border: '2px solid #9f8082', padding:'10px', borderRadius: '8px', backgroundColor: 'white'}}>
-        <img src="headshot.jpeg" style={{ width: '23vw', height: '60vh'}} />
-        <div style={{margin: '5vw'}}>
-        <h1 style={{color: '#70a0af', fontFamily: 'lora', fontWeight: 'bold'}}>Monique Hayes</h1>
-          <p>
-            I'm a former Geometry Teacher turned Full Stack Software Engineer
-            with an eye for detail and a passion for collaborating with others.
-            I've always loved math, logic, and problem solving. After taking on
-            a part-time Operations role during my last year of teaching, I
-            realized I also loved logistics and data, and I wanted to harness
-            this newfound love as I looked to make a career shift. After
-            exploring SQL through an online Cornell course, along with some
-            phenomenal guidance from a mentor of mine, I decided that a Software
-            Engineering bootcamp, and more specifically, the Grace Hopper
-            Program at Fullstack Academy, would be the perfect way to make this
-            shift. Throughout the program I have loved discovering new languages
-            and gaining the knowledge base to create full stack websites in just
-            a few short months. I am most interested in Full Stack and Frontend
-            Development roles. As I enter the job market, I am looking for a
-            company that not only is working to make positive growth and change
-            in the world, but furthermore is passionate about fostering
-            community and growth amongst its employees.
+      <div
+        style={{
+
+          margin: "10vh 0",
+          border: "2px solid #9f8082",
+          padding: "10px",
+          borderRadius: "8px",
+          backgroundColor: "white",
+        }}
+      >
+        <div style={{ margin: "5vw" }}>
+          <h1
+            style={{ color: "#70a0af", fontFamily: "lora", fontWeight: "bold" }}
+          >
+            Monique Hayes
+          </h1>
+          <p style={{fontSize: '1rem'}}>
+            I'm a former Teacher and Operations Manager turned Full Stack
+            Software Engineer with an eye for detail and a passion for
+            collaborating with others. I've always loved math, logic, design,
+            and problem solving.
+            <br />
+            <br />I studied Full Stack Web Development at the Grace Hopper
+            immersive bootcamp through Fullstack Academy. I spent 18 weeks
+            learning full stack web development, collaborating with my fellow
+            cohort members, and creating fully-functional web applications. Some
+            of my work from the program can be viewed{" "}
+            <Link to="/work">here</Link>.
+            <br />
+            <br />I have experience in collaborative, fast-paced environments.
+            In my previous roles it was crucial for me to think on my feet,
+            successfully manage my time between various roles, and quickly
+            respond to data.
+            <br />
+            <br />
+            As I enter the job market, I am interested in working in Full Stack
+            or Frondend development. I am looking for a company that not only is
+            working to make positive growth and change in the world, but
+            furthermore is passionate about fostering community and growth
+            amongst its employees.
           </p>
-          <p>
-            In my free time I enjoy exercising, travelling, spending time with
-            family and friends, and testing out local coffee shops &
-            restaurants. I am currently based in New Orleans, LA.{" "}
-          </p>
+        </div>
+        <div style={{display: 'flex', justifyContent: 'center'}}>
+        <img src="headshot.jpeg" style={{width: '15rem'}}/>
         </div>
       </div>
     </>
